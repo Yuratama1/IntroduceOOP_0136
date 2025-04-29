f03d777b2fe1fa8a3223bb9bc8bc3e6a74954513
Yuratama1 <yuratama661@gmail.com>
2025-04-29 18:35:13 +0700

    membuat Produce printdata

--- introduce.cpp
+++ introduce.cpp
@@ -7,5 +7,12 @@ class mahasiswa{
     string nama;
     float nilai;
 
-   
+    void printdata(){
+        cout << "Masukkan Nim" << nim << endl;
+        cout << "Masukkan Nama" << nama << endl;
+        cout << "Masukkan Nilai" << nilai << endl;
+    }
+
+};
+e
    
